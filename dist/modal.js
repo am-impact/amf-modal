d7f647a1b85f83c928cc93ddaa398166996a621b
Niek Weevers <n.weevers@am-impact.nl>
2017-03-24 08:48:48 +0100

    Scrolling fixes

--- dist/modal.js
+++ dist/modal.js
@@ -1,118 +1,124 @@
 var FW = FW || {};
 
 ;(function($, window, document, undefined) {
-	// Define constructor
-	Modal = function() {
-		this.closeButton = null;
-		this.modal = null;
-		this.overlay = null;
-		this.document = document;
+    // Define constructor
+    Modal = function() {
+        this.closeButton = null;
+        this.modal = null;
+        this.overlay = null;
+        this.document = document;
 
-		// Determine proper prefix
-		this.transitionEnd = transitionSelect();
+        // Determine proper prefix
+        this.transitionEnd = transitionSelect();
 
-		// Create options by extending defaults with the passed in arugments
-		if (arguments[0] && typeof arguments[0] === 'object') {
-			this.options = extendDefaults( Modal.defaults, arguments[0] );
-		}
-	};
+        // Create options by extending defaults with the passed in arugments
+        if (arguments[0] && typeof arguments[0] === 'object') {
+            this.options = extendDefaults( Modal.defaults, arguments[0] );
+        }
+    };
 
-	/**
-	 * Defaults
-	 */
-	Modal.defaults = {
-		closeButton: true,
+    /**
+     * Defaults
+     */
+    Modal.defaults = {
+        closeButton: true,
         closeOnOverlay: true,
         closeOnEscape: true,
-		confirmOnClose: false,
-		confirmOnCloseText: 'Weet u zeker dat u dit venster wilt sluiten?',
+        confirmOnClose: false,
+        confirmOnCloseText: 'Weet u zeker dat u dit venster wilt sluiten?',
         content: '',
         copyFormValues: true,
         maxWidth: null,
         minWidth: null,
         overlay: true,
-		onClose: null,
-		onOpen: null
-	};
+        onClose: null,
+        onOpen: null
+    };
 
 
-	/**
-	 *
-	 * Public Methods
-	 *
-	 */
+    /**
+     *
+     * Public Methods
+     *
+     */
 
-	/**
-	 * Close Modal
-	 */
-	Modal.prototype.close = function() {
-		var self = this;
+    /**
+     * Close Modal
+     */
+    Modal.prototype.close = function() {
+        var self = this;
 
-		/**
-		 * Confirm box when closing modal
-		 */
-		if( this.options.confirmOnClose && !window.confirm( this.options.confirmOnCloseText ) ) {
-			return;
-		}
+        // Remove body position
+        document.body.style.removeProperty('top');
+
+        /**
+         * Confirm box when closing modal
+         */
+        if( this.options.confirmOnClose && !window.confirm( this.options.confirmOnCloseText ) ) {
+            return;
+        }
 
         if( this.options.copyFormValues ) {
             copyFormValues.call(this, 'close');
         }
 
-		this.modal.className = this.modal.className.replace(' modal--open', '');
-		this.overlay.className = this.overlay.className.replace(' modal__overlay--open', '');
-		document.documentElement.className = document.documentElement.className.replace(' modal-opened', '');
+        this.modal.className = this.modal.className.replace(' modal--open', '');
+        this.overlay.className = this.overlay.className.replace(' modal__overlay--open', '');
+        document.documentElement.className = document.documentElement.className.replace(' modal-opened', '');
 
-		// After CSS transition remove Modal and Overlay from DOM
-		this.modal.addEventListener(this.transitionEnd, function() {
-			if( self.modal.parentNode ) {
-				self.modal.parentNode.removeChild(self.modal);
-			}
-		});
+        // After CSS transition remove Modal and Overlay from DOM
+        this.modal.addEventListener(this.transitionEnd, function() {
+            if( self.modal.parentNode ) {
+                self.modal.parentNode.removeChild(self.modal);
+            }
+        });
 
-		this.overlay.addEventListener(this.transitionEnd, function() {
-			if (self.overlay.parentNode) {
-				self.overlay.parentNode.removeChild(self.overlay);
-			}
-		});
+        this.overlay.addEventListener(this.transitionEnd, function() {
+            if (self.overlay.parentNode) {
+                self.overlay.parentNode.removeChild(self.overlay);
+            }
+        });
 
-		/**
-		 * Close callback
-		 */
-		if (typeof this.options.onClose === 'function') {
-			this.options.onClose.call(this);
-		}
-	};
+        /**
+         * Close callback
+         */
+        if (typeof this.options.onClose === 'function') {
+            this.options.onClose.call(this);
+        }
+    };
 
-	/**
-	 * Open Modal
-	 */
-	Modal.prototype.open = function() {
-		buildModal.call(this);
+    /**
+     * Open Modal
+     */
+    Modal.prototype.open = function() {
+        // Set body position
+        document.body.style.top = '-' + document.body.scrollTop + 'px';
+
+        buildModal.call(this);
 
         if( this.options.copyFormValues ) {
             copyFormValues.call(this, 'open');
         }
 
-		// Bind events
-		bindEvents.call(this);
+        // Bind events
+        bindEvents.call(this);
 
-		// After adding elements to the DOM, use getComputedStyle
-		// to force the browser to recalc and recognize the elements
-		// that we just added. This is so that CSS animation has a start point
-		window.getComputedStyle(this.modal).height;
+        // After adding elements to the DOM, use getComputedStyle
+        // to force the browser to recalc and recognize the elements
+        // that we just added. This is so that CSS animation has a start point
+        window.getComputedStyle(this.modal).height;
 
-		this.modal.className = this.modal.className + ' modal--open';
-		this.overlay.className = this.overlay.className + ' modal__overlay--open';
-		document.documentElement.className = document.documentElement.className + ' modal-opened';
+        this.modal.className = this.modal.className + ' modal--open';
+        this.overlay.className = this.overlay.className + ' modal__overlay--open';
+        document.documentElement.className = document.documentElement.className + ' modal-opened';
 
-		/**
-		 * Open callback
-		 */
-		if (typeof this.options.onOpen === 'function') {
-			this.options.onOpen.call(this);
-		}
-	};
+        /**
+         * Open callback
+         */
+        if (typeof this.options.onOpen === 'function') {
+            this.options.onOpen.call(this);
+        }
+    };
 
     /**
      * Fill modal with content
@@ -141,57 +147,57 @@ var FW = FW || {};
     };
 
 
-	/**
-	 *
-	 * Private methods
-	 *
-	 */
+    /**
+     *
+     * Private methods
+     *
+     */
 
-	/**
-	 * Build Modal
-	 */
-	function buildModal() {
-		var contentHolder, docFrag;
+    /**
+     * Build Modal
+     */
+    function buildModal() {
+        var contentHolder, docFrag;
 
-		// Create element
-		docFrag = document.createDocumentFragment();
+        // Create element
+        docFrag = document.createDocumentFragment();
 
-		// Modal
-		this.modal = document.createElement('div');
-		this.modal.className = 'modal';
-		if (this.options.minWidth) {
-			this.modal.style.minWidth = this.options.minWidth + 'px';
-		}
-		if (this.options.maxWidth) {
-			this.modal.style.maxWidth = this.options.maxWidth + 'px';
-		}
+        // Modal
+        this.modal = document.createElement('div');
+        this.modal.className = 'modal';
+        if (this.options.minWidth) {
+            this.modal.style.minWidth = this.options.minWidth + 'px';
+        }
+        if (this.options.maxWidth) {
+            this.modal.style.maxWidth = this.options.maxWidth + 'px';
+        }
 
-		if (this.options.closeButton === true) {
-			this.closeButton = document.createElement('span');
-			this.closeButton.className = 'modal__close';
-			this.modal.appendChild(this.closeButton);
-		}
+        if (this.options.closeButton === true) {
+            this.closeButton = document.createElement('span');
+            this.closeButton.className = 'modal__close';
+            this.modal.appendChild(this.closeButton);
+        }
 
-		if (this.options.overlay === true) {
-			this.overlay = document.createElement('div');
-			this.overlay.className = 'modal__overlay';
-			docFrag.appendChild(this.overlay);
-		}
+        if (this.options.overlay === true) {
+            this.overlay = document.createElement('div');
+            this.overlay.className = 'modal__overlay';
+            docFrag.appendChild(this.overlay);
+        }
 
-		// Content in Modal
-		contentHolder = document.createElement('div');
-		contentHolder.className = 'modal__content';
+        // Content in Modal
+        contentHolder = document.createElement('div');
+        contentHolder.className = 'modal__content';
 
         this.setContent( this.options.content, null, contentHolder );
 
-		this.modal.appendChild(contentHolder);
+        this.modal.appendChild(contentHolder);
 
-		// Add Modal to element
-		docFrag.appendChild(this.modal);
+        // Add Modal to element
+        docFrag.appendChild(this.modal);
 
-		// Add element to body
-		document.body.appendChild(docFrag);
-	}
+        // Add element to body
+        document.body.appendChild(docFrag);
+    }
 
     /**
      * Copy form values from origin to modal or vice versa
@@ -246,62 +252,62 @@ var FW = FW || {};
         }
     }
 
-	/**
-	 * Bind events
-	 */
-	function bindEvents() {
-		var self = this;
+    /**
+     * Bind events
+     */
+    function bindEvents() {
+        var self = this;
 
-		if (this.closeButton) {
-			this.closeButton.addEventListener('click', this.close.bind(this));
-		}
+        if (this.closeButton) {
+            this.closeButton.addEventListener('click', this.close.bind(this));
+        }
 
-		if (this.overlay && this.options.closeOnOverlay) {
-			this.overlay.addEventListener('click', this.close.bind(this));
-		}
+        if (this.overlay && this.options.closeOnOverlay) {
+            this.overlay.addEventListener('click', this.close.bind(this));
+        }
 
-		this.document.addEventListener('keydown', function(e) {
+        this.document.addEventListener('keydown', function(e) {
             // Escape
-			if(
+            if(
                 e.which === 27 &&
                 self.options.closeOnEscape &&
                 self.modal.className.indexOf('modal--open') > -1
             ) {
-				self.close();
-			}
-		});
-	}
+                self.close();
+            }
+        });
+    }
 
-	/**
-	 * Utility method to extend defaults with user options
-	 */
-	function extendDefaults(source, properties) {
-		var property;
+    /**
+     * Utility method to extend defaults with user options
+     */
+    function extendDefaults(source, properties) {
+        var property;
 
-		for (property in properties) {
-			if (properties.hasOwnProperty(property)) {
-				source[property] = properties[property];
-			}
-		}
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
 
-		return source;
-	}
+        return source;
+    }
 
-	/**
-	 * Utility method to determine which transistionend event is supported
-	 */
-	function transitionSelect() {
-		var el = document.createElement("div");
-		if (el.style.WebkitTransition) return "webkitTransitionEnd";
-		if (el.style.OTransition) return "oTransitionEnd";
-		return 'transitionend';
-	}
+    /**
+     * Utility method to determine which transistionend event is supported
+     */
+    function transitionSelect() {
+        var el = document.createElement("div");
+        if (el.style.WebkitTransition) return "webkitTransitionEnd";
+        if (el.style.OTransition) return "oTransitionEnd";
+        return 'transitionend';
+    }
 
-	/**
-	 * Default elements which opens a Modal
-	 *  - uses jQuery
-	 */
-	if( typeof jQuery !== 'undefined' ) {
+    /**
+     * Default elements which opens a Modal
+     *  - uses jQuery
+     */
+    if( typeof jQuery !== 'undefined' ) {
         var amfModal;
 
         jQuery('body').on('click', '[data-openmodal]', function() {
@@ -324,8 +330,8 @@ var FW = FW || {};
         });
     }
 
-	/**
-	 * Add Modal to FW object
-	 */
-	FW.Modal = Modal;
+    /**
+     * Add Modal to FW object
+     */
+    FW.Modal = Modal;
 })(window.Zepto || window.jQuery, window, document);
